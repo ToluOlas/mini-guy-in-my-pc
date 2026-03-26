384ad9682d7377db98c400f214b31f5bdba308ac
TD <tolueolasupo@gmail.com>
2026-03-26 18:35:10 +0000

    Added setup for the development of the search sub agent

---
+++ search-agent/searchAgent.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+from typing import Annotated
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langchain.chat_models import init_chat_model
+from typing_extensions import TypedDict
+from pydantic import BaseModel, Field
+
+load_dotenv()
+
+llm = init_chat_model("gpt-4o")
